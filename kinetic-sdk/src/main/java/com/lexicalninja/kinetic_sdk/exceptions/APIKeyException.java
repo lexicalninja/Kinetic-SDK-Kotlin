6b7794ab06e98a839125917cbe9973e2291b0d9e
Patrick Saxton <patrick@urban-intellect.com>
2020-04-13 15:15:35 -0500

    add APIKeyException class

---
+++ kinetic-sdk/src/main/java/com/lexicalninja/kinetic_sdk/exceptions/APIKeyException.java
@@ -0,0 +1,8 @@
+package com.lexicalninja.kinetic_sdk.exceptions;
+
+
+public class APIKeyException extends Exception {
+    public APIKeyException(String detailMessage) {
+        super(detailMessage);
+    }
+}
