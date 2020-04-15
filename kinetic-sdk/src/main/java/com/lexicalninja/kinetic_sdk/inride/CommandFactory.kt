3939658f17aae43d67287f3cfc65653955b605ef
Patrick Saxton <patrick@urban-intellect.com>
2020-04-15 14:28:43 -0500

    wip

--- kinetic-sdk/src/main/java/com/lexicalninja/kinetic_sdk/inride/CommandFactory.kt
+++ kinetic-sdk/src/main/java/com/lexicalninja/kinetic_sdk/inride/CommandFactory.kt
@@ -23,130 +23,131 @@ class CommandFactory {
         const val SetSpindownTime: Byte = 0x05
     }
 
-    @Throws(InvalidDataException::class)
-    fun StartCalibrationCommandData(systemId: ByteArray): ByteArray {
-        if (systemId.size != 6) {
-            throw InvalidDataException("Invalid System Id")
-        }
-        val key = CommandKeyForSystemId(systemId)
-        val command = ByteArray(3)
-        command[0] = key[0]
-        command[1] = key[1]
-        command[2] = SensorCommands.StartCalibration
-        return command
-    }
-
-    @Throws(InvalidDataException::class)
-    fun StopCalibrationCommandData(systemId: ByteArray): ByteArray {
-        if (systemId.size != 6) {
-            throw InvalidDataException("Invalid System Id")
-        }
-        val key = CommandKeyForSystemId(systemId)
-        val command = ByteArray(3)
-        command[0] = key[0]
-        command[1] = key[1]
-        command[2] = SensorCommands.StopCalibration
-        return command
-    }
-
-    @Throws(InvalidDataException::class)
-    fun SetSpindownTimeCommandData(seconds: Double, systemId: ByteArray): ByteArray {
-        if (systemId.size != 6) {
-            throw InvalidDataException("Invalid System Id")
-        }
-        val key = CommandKeyForSystemId(systemId)
-        val command = ByteArray(7)
-        command[0] = key[0]
-        command[1] = key[1]
-        command[2] = SensorCommands.SetSpindownTime
-        val spindownTicks = (seconds * 32768).toInt()
-        //        byte[] spindownBytes = BitConverter.GetBytes(spindownTicks);
-        command[3] = (spindownTicks shr 0).toByte()
-        command[4] = (spindownTicks shr 8).toByte()
-        command[5] = (spindownTicks shr 16).toByte()
-        command[6] = (spindownTicks shr 24).toByte()
-        return command
-    }
-
-    @Throws(InvalidDataException::class)
-    fun ConfigureSensorCommandData(updateRate: SensorUpdateRate?, systemId: ByteArray): ByteArray {
-        if (systemId.size != 6) {
-            throw InvalidDataException("Invalid System Id")
-        }
-        val key = CommandKeyForSystemId(systemId)
-        val command = ByteArray(15)
-        command[0] = key[0]
-        command[1] = key[1]
-        command[2] = SensorCommands.SetSpindownParams
-        command[3] = (CalibrationParameters.CalibrationReady shr 0).toByte()
-        command[4] = (CalibrationParameters.CalibrationReady shr 8).toByte()
-        command[5] = (CalibrationParameters.CalibrationStart shr 0).toByte()
-        command[6] = (CalibrationParameters.CalibrationStart shr 8).toByte()
-        command[7] = (CalibrationParameters.CalibrationEnd shr 0).toByte()
-        command[8] = (CalibrationParameters.CalibrationEnd shr 8).toByte()
-        command[9] = (CalibrationParameters.CalibrationDebounce shr 0).toByte()
-        command[10] = (CalibrationParameters.CalibrationDebounce shr 8).toByte()
-        var updateDefault = 32
-        when (updateRate) {
-            SensorUpdateRate.Millis1000 -> {
-                updateDefault = 32
+    companion object {
+        @Throws(InvalidDataException::class)
+        fun StartCalibrationCommandData(systemId: ByteArray): ByteArray {
+            if (systemId.size != 6) {
+                throw InvalidDataException("Invalid System Id")
             }
-            SensorUpdateRate.Millis500 -> {
-                updateDefault = 16
-            }
-            SensorUpdateRate.Millis250 -> {
-                updateDefault = 8
-            }
+            val key = CommandKeyForSystemId(systemId)
+            val command = ByteArray(3)
+            command[0] = key[0]
+            command[1] = key[1]
+            command[2] = SensorCommands.StartCalibration
+            return command
         }
-        command[11] = (updateDefault shr 0).toByte()
-        command[12] = (updateDefault shr 8).toByte()
-        var updateFast = 8
-        updateFast = when (CalibrationParameters.UpdateIntervalFast) {
-            SensorUpdateRate.Millis1000 -> {
-                32
+
+        @Throws(InvalidDataException::class)
+        fun StopCalibrationCommandData(systemId: ByteArray): ByteArray {
+            if (systemId.size != 6) {
+                throw InvalidDataException("Invalid System Id")
             }
-            SensorUpdateRate.Millis500 -> {
-                16
-            }
-            SensorUpdateRate.Millis250 -> {
-                8
-            }
+            val key = CommandKeyForSystemId(systemId)
+            val command = ByteArray(3)
+            command[0] = key[0]
+            command[1] = key[1]
+            command[2] = SensorCommands.StopCalibration
+            return command
         }
-        command[13] = (updateFast shr 0).toByte()
-        command[14] = (updateFast shr 8).toByte()
-        return command
+
+        @Throws(InvalidDataException::class)
+        fun SetSpindownTimeCommandData(seconds: Double, systemId: ByteArray): ByteArray {
+            if (systemId.size != 6) {
+                throw InvalidDataException("Invalid System Id")
+            }
+            val key = CommandKeyForSystemId(systemId)
+            val command = ByteArray(7)
+            command[0] = key[0]
+            command[1] = key[1]
+            command[2] = SensorCommands.SetSpindownTime
+            val spindownTicks = (seconds * 32768).toInt()
+            //        byte[] spindownBytes = BitConverter.GetBytes(spindownTicks);
+            command[3] = (spindownTicks shr 0).toByte()
+            command[4] = (spindownTicks shr 8).toByte()
+            command[5] = (spindownTicks shr 16).toByte()
+            command[6] = (spindownTicks shr 24).toByte()
+            return command
+        }
+
+        @Throws(InvalidDataException::class)
+        fun ConfigureSensorCommandData(updateRate: SensorUpdateRate?, systemId: ByteArray): ByteArray {
+            if (systemId.size != 6) {
+                throw InvalidDataException("Invalid System Id")
+            }
+            val key = CommandKeyForSystemId(systemId)
+            val command = ByteArray(15)
+            command[0] = key[0]
+            command[1] = key[1]
+            command[2] = SensorCommands.SetSpindownParams
+            command[3] = (CalibrationParameters.CalibrationReady shr 0).toByte()
+            command[4] = (CalibrationParameters.CalibrationReady shr 8).toByte()
+            command[5] = (CalibrationParameters.CalibrationStart shr 0).toByte()
+            command[6] = (CalibrationParameters.CalibrationStart shr 8).toByte()
+            command[7] = (CalibrationParameters.CalibrationEnd shr 0).toByte()
+            command[8] = (CalibrationParameters.CalibrationEnd shr 8).toByte()
+            command[9] = (CalibrationParameters.CalibrationDebounce shr 0).toByte()
+            command[10] = (CalibrationParameters.CalibrationDebounce shr 8).toByte()
+            var updateDefault = 32
+            when (updateRate) {
+                SensorUpdateRate.Millis1000 -> {
+                    updateDefault = 32
+                }
+                SensorUpdateRate.Millis500 -> {
+                    updateDefault = 16
+                }
+                SensorUpdateRate.Millis250 -> {
+                    updateDefault = 8
+                }
+            }
+            command[11] = (updateDefault shr 0).toByte()
+            command[12] = (updateDefault shr 8).toByte()
+            var updateFast = 8
+            updateFast = when (CalibrationParameters.UpdateIntervalFast) {
+                SensorUpdateRate.Millis1000 -> {
+                    32
+                }
+                SensorUpdateRate.Millis500 -> {
+                    16
+                }
+                SensorUpdateRate.Millis250 -> {
+                    8
+                }
+            }
+            command[13] = (updateFast shr 0).toByte()
+            command[14] = (updateFast shr 8).toByte()
+            return command
+        }
+
+        @Throws(InvalidDataException::class)
+        fun SetPeripheralNameCommandData(name: String, systemId: ByteArray): ByteArray {
+            if (systemId.size != 6) {
+                throw InvalidDataException("Invalid System Id")
+            }
+            val nameBytes = name.toByteArray()
+            if (nameBytes.size < 3 || nameBytes.size > 8) {
+                throw InvalidDataException("Peripheral name must be between 3 and 8 characters")
+            }
+            val key = CommandKeyForSystemId(systemId)
+            val command = ByteArray(3 + nameBytes.size)
+            command[0] = key[0]
+            command[1] = key[1]
+            command[2] = SensorCommands.SetName
+            System.arraycopy(nameBytes, 0, command, 3, nameBytes.size)
+            return command
+        }
+
+        @Throws(InvalidDataException::class)
+        private fun CommandKeyForSystemId(systemId: ByteArray): ByteArray {
+            if (systemId.size != 6) {
+                throw InvalidDataException("Invalid System Id")
+            }
+            val sysidx1: Int = (systemId[3].toInt() and 0xFF) % 6
+            val sysidx2: Int = (systemId[5].toInt() and 0xFF) % 6
+            val commandKey = ByteArray(2)
+            commandKey[0] = systemId[sysidx1]
+            commandKey[1] = systemId[sysidx2]
+            return commandKey
+        }
+
     }
-
-    @Throws(InvalidDataException::class)
-    fun SetPeripheralNameCommandData(name: String, systemId: ByteArray): ByteArray {
-        if (systemId.size != 6) {
-            throw InvalidDataException("Invalid System Id")
-        }
-        val nameBytes = name.toByteArray()
-        if (nameBytes.size < 3 || nameBytes.size > 8) {
-            throw InvalidDataException("Peripheral name must be between 3 and 8 characters")
-        }
-        val key = CommandKeyForSystemId(systemId)
-        val command = ByteArray(3 + nameBytes.size)
-        command[0] = key[0]
-        command[1] = key[1]
-        command[2] = SensorCommands.SetName
-        System.arraycopy(nameBytes, 0, command, 3, nameBytes.size)
-        return command
-    }
-
-    @Throws(InvalidDataException::class)
-    private fun CommandKeyForSystemId(systemId: ByteArray): ByteArray {
-        if (systemId.size != 6) {
-            throw InvalidDataException("Invalid System Id")
-        }
-        val sysidx1: Int = (systemId[3].toInt() and 0xFF) % 6
-        val sysidx2: Int = (systemId[5].toInt() and 0xFF) % 6
-        val commandKey = ByteArray(2)
-        commandKey[0] = systemId[sysidx1]
-        commandKey[1] = systemId[sysidx2]
-        return commandKey
-    }
-
-
 }
